dfc0124104085b74dc8e14837913e0826978b2f3
Alex Gaesser <agaesser@gmail.com>
2017-04-10 14:11:21 -0700

    modified spells for how I think accessing attributes works. This does not take advatage of the JSON formatting from kadyn

--- src/spells.js
+++ src/spells.js
@@ -2,5 +2,33 @@ module.exports = {
     "SPELLS_EN_US" : {
     	"acid splash" : "conjuration cantrip. Casting time: one action. Range: 60 feet. Components: Verbal, Somatic. Duration: Instantaneous. You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within five feet of each other. A target must succceed on a Dexterity saving throw or take 1 d 6 acid damage. This spell's damage increases by 1d6 when you reach 5th level, 11th level, and 17th level.",
     	"aid" : "2nd level abjuration. Casting time: one action. Range: 30 feet. Components: Verbal, Somatic, Material (a tiny strip of white cloth). Duration: 8 hours. Your spell bolsters your allies with toughness and resolve. Choose up to three creatures within range. Each target's hit point maximum and current hit points increase by 5 for the duration. At higher levels: When you cast this spell using a spell slot of third level or higher, a target's hit points increase by an additional 5 for each slot level above second."
+    },
+    "SPELL_CASTING_TIME_EN_US" : {
+    	"acid splash" : "one action",
+    	"aid" : "one action"
+    },
+    "SPELL_RANGE_EN_US" : {
+    	"acid splash" : "60 feet",
+    	"aid" : "30 feet"
+    },
+    "SPELL_TYPE_EN_US" : {
+    	"acid splash" : "conjuration cantrip",
+    	"aid" : "2nd level abjuration"
+    },
+    "SPELL_COMPONENTS_EN_US" : {
+    	"acid splash" : "Verbal, Somatic",
+    	"aid" : "Verbal, Somatic, Material - A tiny strip of white cloth"
+    },
+    "SPELL_DURATION_EN_US" : {
+    	"acid splash" : "Instantaneous",
+    	"aid" : "8 hours"
+    },
+    "SPELL_SHORT_DESCRIPTION_EN_US" : {
+    	"acid splash" : "Up to two creatures within 5 feet of each other must make a dex save or take 1d6 acid damage.",
+    	"aid" : "Increase current and max hp by five for up to three creatures."
+    },
+    "SPELL_FULL_DESCRIPTION_EN_US" : {
+    	"acid splash" : "You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within five feet of each other. A target must succceed on a Dexterity saving throw or take 1 d 6 acid damage. This spell's damage increases by 1d6 when you reach 5th level, 11th level, and 17th level.",
+    	"aid" : "Your spell bolsters your allies with toughness and resolve. Choose up to three creatures within range. Each target's hit point maximum and current hit points increase by 5 for the duration. At higher levels: When you cast this spell using a spell slot of third level or higher, a target's hit points increase by an additional 5 for each slot level above second."
     }
 };
