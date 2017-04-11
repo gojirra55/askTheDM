18c4b38091982c6f2e5614e5cf08103096014e45
Alex Gaesser <agaesser@gmail.com>
2017-04-11 11:20:30 -0700

    small changes

--- src/spells.js
+++ src/spells.js
@@ -27,7 +27,7 @@ module.exports = {
     	"acid splash" : "Up to two creatures within 5 feet of each other must make a dex save or take 1d6 acid damage.",
     	"aid" : "Increase current and max hp by five for up to three creatures."
     },
-    "SPELL_FULL_DESCRIPTION_EN_US" : {
+    "SPELL_LONG_DESCRIPTION_EN_US" : {
     	"acid splash" : "You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within five feet of each other. A target must succceed on a Dexterity saving throw or take 1 d 6 acid damage. This spell's damage increases by 1d6 when you reach 5th level, 11th level, and 17th level.",
     	"aid" : "Your spell bolsters your allies with toughness and resolve. Choose up to three creatures within range. Each target's hit point maximum and current hit points increase by 5 for the duration. At higher levels: When you cast this spell using a spell slot of third level or higher, a target's hit points increase by an additional 5 for each slot level above second."
     }
